c0d86de1dd10daccab34fd6734523d7cf330c1a7
Dell <Dell@DESKTOP-TMDV7FV>
2023-01-21 06:07:22 -0800

    Add project files.

---
+++ Lab_08/Studentinfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_08
+{
+    class Studentinfo
+    {
+        public int StudentID { get; set; }
+        public String StudentName { get; set; }
+        public int Age { get; set; }
+        public int marks { get; set; }
+        public String gender { get; set; }
+    }
+}
